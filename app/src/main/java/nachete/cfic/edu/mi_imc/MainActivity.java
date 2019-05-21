cd56f42576a5709c30c3ded4ec10bf7b7de2f812
Nachete <josegonfer1977@gmail.com>
2019-05-21 13:45:44 +0200

    Añadido soporte info a wikipedia

--- app/src/main/java/nachete/cfic/edu/mi_imc/MainActivity.java
+++ app/src/main/java/nachete/cfic/edu/mi_imc/MainActivity.java
@@ -1,9 +1,14 @@
 package nachete.cfic.edu.mi_imc;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,6 +18,8 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String URL_BUSCADOR = "https://www.google.com/search?q=";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.mipmap.ic_launcher);
 
-        if (savedInstanceState==null)
-        {
+        if (savedInstanceState == null) {
             Log.d("MIAPP", "Es la primera vez que se ejecuta");
-        }else {
+        } else {
             Log.d("MIAPP", "No es la primera ejecucion");
         }
     }
@@ -58,8 +64,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MIAPP", "Estoy en ondestroy");
     }
 
-    public void calcular_Imc (View view)
-    {
+    public void calcular_Imc(View view) {
         //TODO GESTIONAR EL EVENTO DEL BOTÓN
         // Recoger peso y altura
         EditText editText = this.findViewById(R.id.editText_peso);
@@ -72,18 +77,18 @@ public class MainActivity extends AppCompatActivity {
 
         // calcular imc
 
-        Float imc = (peso/(altura*altura));
-        Float imc_en = imc*703;
+        Float imc = (peso / (altura * altura));
+        Float imc_en = imc * 703;
 
         // Mostrar IMC y Resultado
 
         String idioma = Locale.getDefault().getLanguage();
         TextView textView = findViewById(R.id.textView_imc);
 
-        if (idioma =="en") {
+        if (idioma == "en") {
             textView.setText(imc_en.toString());
             imc = imc_en;
-        }else {
+        } else {
             textView.setText(imc.toString());
         }
 
@@ -92,21 +97,42 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
 
-        if (imc<=18){
+        if (imc <= 18) {
             imageView.setImageResource(R.drawable.imc_desnutrido);
             textView1.setText(getString(R.string.desnutrido));
-        }else if (imc<=25){
+        } else if (imc <= 25) {
             imageView.setImageResource(R.drawable.imc_delgado);
             textView1.setText(getString(R.string.delgado));
-        }else if (imc<=30){
+        } else if (imc <= 30) {
             imageView.setImageResource(R.drawable.imc_obeso);
             textView1.setText(getString(R.string.sobrepeso));
-        }else {
+        } else {
             imageView.setImageResource(R.drawable.icm_sobrepeso);
             textView1.setText(getString(R.string.obeso));
         }
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        buscar();
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void buscar() {
+        String query = "https://es.wikipedia.org/wiki/%C3%8Dndice_de_masa_corporal";
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.putExtra(SearchManager.QUERY, query);
+        if (intent.resolveActivity(getPackageManager()) != null) // Si hay alguna actividad compatible con esa acción
+        {
+            startActivity(intent);
+        }
+    }
 }
